2c52e3df4457ecc6a04c5c28687e7db591247a60
Leo Cassarani <me@leocassarani.net>
2020-02-09 20:05:45 +0000

    SB_IO

--- Computer.v
+++ Computer.v
@@ -1,6 +1,7 @@
 module Computer (
   input CLK,
-  input P1B1, P1B3,
+  inout P1B1, P1B3,
+  input BTN1,
   output P1A1, P1A2, P1A3, P1A4, P1A7, P1A8, P1A9, P1A10,
   output LEDR_N,
 );
@@ -18,7 +19,7 @@ ps2_receiver ps2 (
   .ps2_clk(P1B3),
   .ps2_data(P1B1),
   .out(ps2_out),
-  .ready(LEDR_N),
+  .idle_out(LEDR_N),
 );
 
 endmodule
--- ps2.v
+++ ps2.v
@@ -1,8 +1,9 @@
 module ps2_receiver (
   input clk,
-  input ps2_clk, ps2_data,
+  inout ps2_clk, ps2_data,
+  input tx,
   output reg [7:0] out,
-  output reg ready,
+  output reg idle_out,
   output reg parity,
 );
 
@@ -22,16 +23,72 @@ reg [7:0] ps2_clk_debounce = 8'b10101010;
 wire ps2_clk_high = ps2_clk_debounce[7:3] == 5'b11111;
 wire ps2_clk_low = ps2_clk_debounce[7:3] == 5'b0000;
 
+reg [9:0] clkdiv;
+reg clkpulse;
+
+parameter [10:0] txmsg = 11'b0_1111_1111_11;
+reg [3:0] txdata;
+
+// 0xfe = 0 0111 1111 01
+
 always @(posedge clk) begin
-  ps2_clk_debounce <= { ps2_clk_debounce[6:0], ps2_clk };
+  if (clkdiv == 10'd1000) begin
+    clkdiv <= 0;
+    clkpulse <= 1;
+
+    if (!tx) begin
+      txdata <= 0;
+    end else if (txdata == 10) begin
+      txdata <= 0;
+    end else begin
+      txdata <= txdata + 1;
+    end
+  end else begin
+    clkdiv <= clkdiv + 1;
+    clkpulse <= 0;
+  end
+end
+
+wire ps2_data_tx = txmsg[txdata];
+
+wire ps2_clk_rw;
+wire ps2_data_rw;
+
+SB_IO #(
+  .PIN_TYPE(6'b1010_00),
+  .PULLUP(1),
+) ps2_clk_io (
+  .PACKAGE_PIN(ps2_clk),
+  .INPUT_CLK(clk),
+  .OUTPUT_CLK(clk),
+  .OUTPUT_ENABLE(tx),
+  .D_IN_0(ps2_clk_rw),
+  .D_OUT_0(clkpulse),
+);
+
+SB_IO #(
+  .PIN_TYPE(6'b1010_00),
+  .PULLUP(1),
+) ps2_data_io (
+  .PACKAGE_PIN(ps2_data),
+  .INPUT_CLK(clk),
+  .OUTPUT_CLK(clk),
+  .OUTPUT_ENABLE(tx),
+  .D_IN_0(ps2_data_rw),
+  .D_OUT_0(ps2_data_tx),
+);
+
+always @(posedge clk) begin
+  ps2_clk_debounce <= { ps2_clk_debounce[6:0], ps2_clk_rw };
 
   case (state)
     idle: begin
       rx_count <= 0;
+      idle_out <= 1;
 
       // If the device pulls the clock line low, start receiving.
       if (ps2_clk_low) begin
-        ready <= 0;
+        idle_out <= 0;
         state <= rx_down_edge;
       end
     end
@@ -40,7 +97,6 @@ always @(posedge clk) begin
       if (rx_count == BITS_PER_FRAME) begin
         // TODO: check parity and go back to idle if an error has occurred.
         out <= frame[8:1];
-        ready <= 1;
         state <= idle;
       end else if (ps2_clk_low) begin
         state <= rx_down_edge;
@@ -55,7 +111,7 @@ always @(posedge clk) begin
 
     rx_down_edge: begin
       // Shift to the right so that by the end, the first bit becomes the LSB.
-      frame <= { ps2_data, frame[10:1] };
+      frame <= { ps2_data_rw, frame[10:1] };
       rx_count <= rx_count + 1'b1;
       state <= rx_clk_low;
     end
