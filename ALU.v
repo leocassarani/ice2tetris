1eaf3b8a0d028a33d0eae43e736da8f138e93560
Leo Cassarani <me@leocassarani.net>
2020-03-11 12:04:53 +0000

    Implement a simple CPU and ALU without jumps or memory access
    This simple CPU is capable of executing both A- and C-instructions, as
    long as all the operands and destinations are the A and D registers,
    rather than a location in RAM. In addition, the rudimentary program
    counter is incremented at every clock cycle, without any support for
    conditional or unconditional jumps that may be specified using the least
    significant three bits of a C-instruction.

---
+++ ALU.v
@@ -0,0 +1,22 @@
+`default_nettype none
+
+module ALU (
+  input [15:0] x,
+  input [15:0] y,
+  input zx, nx, zy, ny, f, no,
+  output [15:0] out,
+  output zero, neg,
+);
+
+wire [15:0] x_result, y_result, result;
+
+assign x_result = zx ? (nx ? ~16'b0 : 16'b0) : (nx ? ~x : x);
+assign y_result = zy ? (ny ? ~16'b0 : 16'b0) : (ny ? ~y : y);
+
+assign result = f ? x_result + y_result : x_result & y_result;
+assign out = no ? ~result : result;
+
+assign zero = ~|out; // zero = NOR(out)
+assign neg = out[15];
+
+endmodule
--- Computer.v
+++ Computer.v
@@ -2,7 +2,6 @@
 
 module Computer (
   input CLK,
-  input BTN1,
   input FLASH_IO1,
   output LEDR_N, LEDG_N,
   output P1A1, P1A2, P1A3, P1A4, P1A7, P1A8, P1A9, P1A10,
@@ -11,11 +10,11 @@ module Computer (
 );
 
 wire clk_out, clk_locked;
-wire reset = !clk_locked;
 wire rom_ready;
 
-reg [15:0] rom_address = 0;
+wire [15:0] rom_address;
 wire [15:0] instruction;
+wire [15:0] a_reg;
 
 assign LEDR_N = !clk_locked;
 assign LEDG_N = !rom_ready;
@@ -26,9 +25,17 @@ Clock clock (
   .out(clk_out),
 );
 
+CPU cpu (
+  .clk(clk_out),
+  .reset(!rom_ready),
+  .instruction(instruction),
+  .prog_counter(rom_address),
+  .a_reg(a_reg),
+);
+
 ROM rom (
   .clk(clk_out),
-  .reset(reset),
+  .reset(!clk_locked),
   .ready(rom_ready),
   .address(rom_address),
   .instruction(instruction),
@@ -40,13 +47,9 @@ ROM rom (
 
 Screen screen (
   .clk(clk_out),
-  .din(instruction),
+  .din(a_reg),
   .dout_lo({ P1A10, P1A9, P1A8, P1A7, P1A4, P1A3, P1A2, P1A1 }),
   .dout_hi({ P1B10, P1B9, P1B8, P1B7, P1B4, P1B3, P1B2, P1B1 }),
 );
-
-always @(posedge BTN1) begin
-  rom_address <= rom_address + 1;
-end
 
 endmodule
