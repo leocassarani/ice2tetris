9a5ab58d11866cbaf1f6ad892cc809c17e7e79e9
Leo Cassarani <me@leocassarani.net>
2020-03-14 18:12:12 +0000

    Add a rudimentary Keyboard driven by the three-button snap-off PMOD
    According to the Hack specification, reading the contents of memory
    address 0x6000 must return a 16-bit ASCII value representing the key
    that is currently being pressed.
    
    We don't yet support a full hardware keyboard, so for the time being we
    use the snap-off PMOD on the iCEBreaker, which comes with three buttons,
    to simulate left arrow, escape, and right arrow. Although extremely
    limited, this particular set of keys would be enough for a game of Pong.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJ = computer
-ADD_SRC = alu.v cpu.v clock.v memory.v rom.v screen.v
+ADD_SRC = alu.v cpu.v clock.v keyboard.v memory.v ram.v rom.v screen.v
 
 PIN_DEF = icebreaker.pcf
 DEVICE = up5k
---
+++ keyboard.v
@@ -0,0 +1,21 @@
+`default_nettype none
+
+module keyboard (
+  input clk,
+  input [2:0] buttons,
+  output reg [15:0] out,
+);
+
+always @(posedge clk) begin
+  if (buttons[2]) begin
+    out <= 130; // Left arrow
+  end else if (buttons[1]) begin
+    out <= 140; // Escape
+  end else if (buttons[0]) begin
+    out <= 132; // Right arrow
+  end else begin
+    out <= 0;   // No key pressed
+  end
+end
+
+endmodule
