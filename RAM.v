7f76dd233b9ba6984509d75eb6ec177a4de7d543
Leo Cassarani <me@leocassarani.net>
2020-03-14 18:12:12 +0000

    Add a rudimentary Keyboard driven by the three-button snap-off PMOD
    According to the Hack specification, reading the contents of memory
    address 0x6000 must return a 16-bit ASCII value representing the key
    that is currently being pressed.
    
    We don't yet support a full hardware keyboard, so for the time being we
    use the snap-off PMOD on the iCEBreaker, which comes with three buttons,
    to simulate left arrow, escape, and right arrow. Although extremely
    limited, this particular set of keys would be enough for a game of Pong.

---
+++ RAM.v
@@ -0,0 +1,24 @@
+`default_nettype none
+
+module RAM (
+  input clk,
+  input load,
+  input [13:0] address,
+  input [15:0] in,
+  output [15:0] out,
+);
+
+SB_SPRAM256KA ram (
+  .CLOCK(clk),
+  .CHIPSELECT(1'b1),
+  .ADDRESS(address),
+  .WREN(load),
+  .MASKWREN(4'b1111),
+  .DATAIN(in),
+  .STANDBY(1'b0),
+  .SLEEP(1'b0),
+  .POWEROFF(1'b1),
+  .DATAOUT(out),
+);
+
+endmodule
