9d886bd4f0fc555cc18c830fb8a681a20bcf6f3f
Leo Cassarani <me@leocassarani.net>
2020-03-23 00:42:13 +0000

    Implement VGA screen output backed by pseudo dual-port VRAM
    This commit introduces a pseudo dual-port VRAM that is shared between a
    VGA output module and the Hack CPU. The VRAM uses the lowest 16KiB of a
    32KiB SPRAM chip and can be accessed at memory addresses 0x4000-0x5FFF.
    
    The VGA screen has priority, read-only access to the shared VRAM. A
    "read enable" signal is used to request a memory read, which happens
    once every 16 pixels while in the active rendering portion of the
    screen. Based on our 25.125MHz pixel clock, we can render a 640x480
    picture at 60Hz; however, in accordance with the Hack specification, we
    only display a 512x256 black-and-white "letterbox", with pixels falling
    outside of that rectangle set to display the colour black.
    
    When the VGA module is not reading from the VRAM, the "busy" signal goes
    low and the CPU can request a read or write. Due to the shared VRAM
    logic, each read or write incurs a two-cycle delay, which is handled by
    a new mem_read stage in the CPU pipeline. In addition, memory reads are
    cached in a new 16-bit register, which is updated by every instruction
    that modifies the A register: specifically, all A-instructions as well
    as C-instructions with the "d1" flag high. A further mem_fetch CPU state
    is used to transfer data from the memory's output bus to the register.
    
    This commit, along with the three-button keyboard introduced by 1472ada,
    is sufficient to play a full game of Pong using the source code supplied
    with the Nand2Tetris course.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJ = computer
-ADD_SRC = alu.v clock.v cpu.v keyboard.v memory.v ram.v rom.v screen.v
+ADD_SRC = alu.v clock.v cpu.v keyboard.v memory.v ram.v rom.v screen.v vga.v vram.v
 
 PIN_DEF = icebreaker.pcf
 DEVICE = up5k
--- memory.v
+++ memory.v
@@ -4,27 +4,65 @@
 module memory (
   input clk,
   input load,
-  input [2:0] buttons,
   input [15:0] address,
   input [15:0] in,
+
+  output vga_h_sync, vga_v_sync,
+  output [3:0] vga_red, vga_green, vga_blue,
+  input [2:0] kbd_buttons,
+
+  output busy,
   output [15:0] out
 );
 
-wire [15:0] ram_out, keyboard_out;
-assign out = address[14] ? keyboard_out : ram_out;
+wire [15:0] ram_out, screen_out, kbd_out;
+
+wire ram_select_0 = !address[14];
+wire screen_select_0 = address[14] && !address[13];
+
+reg ram_select_1, screen_select_1;
+
+// We need to use the delayed ram_select_1 and screen_select_1 signals in
+// order to match the one-clock delay between requesting a memory read and
+// receiving the results, otherwise we'd run the risk of propagating the
+// output of a memory module that hasn't had enough time to process a read.
+assign out = ram_select_1 ? (
+  ram_out
+) : (
+  screen_select_1 ? screen_out : kbd_out
+);
 
 ram ram (
   .clk(clk),
-  .load(load),
+  .load(ram_select_0 && load),
   .address(address[13:0]),
   .in(in),
   .out(ram_out)
 );
 
+screen screen (
+  .clk(clk),
+  .vram_load(screen_select_0 && load),
+  .vram_addr(address[12:0]),
+  .vram_din(in),
+  .vram_busy(busy),
+  .vram_dout(screen_out),
+  .vga_h_sync(vga_h_sync),
+  .vga_v_sync(vga_v_sync),
+  .vga_red(vga_red),
+  .vga_green(vga_green),
+  .vga_blue(vga_blue)
+);
+
 keyboard keyboard (
   .clk(clk),
-  .buttons(buttons),
-  .out(keyboard_out)
+  .buttons(kbd_buttons),
+  .out(kbd_out)
 );
+
+always @(posedge clk) begin
+  ram_select_1 <= ram_select_0;
+  screen_select_1 <= screen_select_0;
+end
 
 endmodule
