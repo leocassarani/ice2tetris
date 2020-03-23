e10fda9ca8a8e2d06097d78b593665e702f927d2
Leo Cassarani <me@leocassarani.net>
2020-03-23 00:11:09 +0000

    Add a phsyical reset button for the CPU
    Pressing the BTN_N button, located next to the USB port on the
    iCEBreaker board, will cause the CPU to zero out its registers and
    resume execution from the first instruction at ROM address 0.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJ = computer
-ADD_SRC = alu.v cpu.v clock.v keyboard.v memory.v ram.v rom.v screen.v
+ADD_SRC = alu.v cpu.v clock.v keyboard.v memory.v ram.v rom.v screen.v vga.v vram.v
 
 PIN_DEF = icebreaker.pcf
 DEVICE = up5k
--- computer.v
+++ computer.v
@@ -2,22 +2,21 @@
 
 module computer (
   input CLK,
-  input BTN1, BTN2, BTN3,
+  input BTN_N, BTN1, BTN2, BTN3,
   input FLASH_IO1,
   output LEDR_N,
   output P1A1, P1A2, P1A3, P1A4, P1A7, P1A8, P1A9, P1A10,
-  output P1B1, P1B2, P1B3, P1B4, P1B7, P1B8, P1B9, P1B10,
+  output P1B1, P1B2, P1B3, P1B4, P1B7, P1B8,
   output FLASH_SCK, FLASH_SSB, FLASH_IO0,
 );
 
 wire clk_out, clk_locked;
 wire rom_ready;
 
-wire [15:0] a_reg, d_reg;
 wire [15:0] rom_address;
 wire [15:0] instruction;
 wire [15:0] mem_address, mem_rdata, mem_wdata;
-wire mem_write;
+wire mem_busy, mem_load;
 
 assign LEDR_N = !rom_ready;
 
@@ -29,24 +28,31 @@ clock clock (
 
 cpu cpu (
   .clk(clk_out),
-  .reset(!rom_ready),
+  .reset(!rom_ready || !BTN_N),
   .instruction(instruction),
   .prog_counter(rom_address),
+  .mem_busy(mem_busy),
   .mem_rdata(mem_rdata),
   .mem_wdata(mem_wdata),
-  .mem_write(mem_write),
+  .mem_load(mem_load),
   .mem_address(mem_address),
-  .a_reg(a_reg),
-  .d_reg(d_reg),
 );
 
 memory memory (
   .clk(clk_out),
-  .buttons({ BTN3, BTN2, BTN1 }),
   .address(mem_address),
-  .load(mem_write),
+  .load(mem_load),
   .in(mem_wdata),
   .out(mem_rdata),
+  .busy(mem_busy),
+
+  .vga_h_sync(P1B7),
+  .vga_v_sync(P1B8),
+  .vga_red({ P1A4, P1A3, P1A2, P1A1 }),
+  .vga_blue({ P1A10, P1A9, P1A8, P1A7 }),
+  .vga_green({ P1B4, P1B3, P1B2, P1B1 }),
+
+  .kbd_buttons({ BTN3, BTN2, BTN1 }),
 );
 
 rom rom (
@@ -55,17 +61,11 @@ rom rom (
   .ready(rom_ready),
   .address(rom_address),
   .instruction(instruction),
+
   .spi_cs(FLASH_SSB),
   .spi_sclk(FLASH_SCK),
   .spi_mosi(FLASH_IO0),
   .spi_miso(FLASH_IO1),
-);
-
-screen screen (
-  .clk(clk_out),
-  .din(d_reg),
-  .dout_lo({ P1A10, P1A9, P1A8, P1A7, P1A4, P1A3, P1A2, P1A1 }),
-  .dout_hi({ P1B10, P1B9, P1B8, P1B7, P1B4, P1B3, P1B2, P1B1 }),
 );
 
 endmodule
