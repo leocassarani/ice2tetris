46f0c1adf27ed499a2c540df0e191d62de3f4baf
Leo Cassarani <me@leocassarani.net>
2020-03-26 00:04:24 +0000

    [REBASE ME] Reorganise the pipeline!

--- CPU.v
+++ CPU.v
@@ -13,19 +13,23 @@ module CPU (
 
 reg [15:0] a_reg;
 reg [15:0] d_reg;
+reg [15:0] memory;
 
 assign mem_address = a_reg;
-assign mem_load = state == mem_write && d3;
+assign mem_load = state == write_back && d3;
 assign mem_wdata = alu_out;
 
-wire fast_path = a_reg < 16'h4000;
+wire fast_path = !mem_address[14] || mem_address[13];
 
 wire i = instruction[15];
 wire a = instruction[12];
 
 reg c1, c2, c3, c4, c5, c6;
-reg d1, d2, d3;
-reg j1, j2, j3;
+wire d1, d2, d3;
+wire j1, j2, j3;
+
+assign { d1, d2, d3 } = instruction[5:3];
+assign { j1, j2, j3 } = instruction[2:0];
 
 reg [15:0] alu_x, alu_y;
 wire [15:0] alu_out;
@@ -39,7 +43,8 @@ localparam [2:0] inst_fetch  = 3'd0,
                  mem_wait    = 3'd2,
                  mem_read    = 3'd3,
                  write_back  = 3'd4,
-                 mem_write   = 3'd5;
+                 mem_write   = 3'd5,
+                 mem_wait_1  = 3'd6;
 
 reg [2:0] state = inst_fetch;
 
@@ -66,63 +71,56 @@ always @(posedge clk) begin
   end else begin
     case (state)
       inst_fetch: begin
+        memory <= mem_rdata;
         state <= inst_decode;
       end
 
       inst_decode: begin
         if (i) begin // C-instruction
           { c1, c2, c3, c4, c5, c6 } <= instruction[11:6];
-          { d1, d2, d3 } <= instruction[5:3];
-          { j1, j2, j3 } <= instruction[2:0];
-
           alu_x <= d_reg;
-
-          if (!a) begin
-            alu_y <= a_reg;
-            state <= mem_write;
-          end else if (fast_path) begin
-            alu_y <= mem_rdata;
-            state <= mem_write;
-          end else if (!mem_busy) begin
-            state <= mem_wait;
-          end
+          alu_y <= a ? memory : a_reg;
+          state <= write_back;
         end else begin // A-instruction
           a_reg <= instruction;
           prog_counter <= prog_counter + 1;
-          state <= inst_fetch;
-        end
-      end
-
-      mem_wait: begin
-        state <= mem_read;
-      end
-
-      mem_read: begin
-        alu_y <= mem_rdata;
-        state <= mem_write;
-      end
-
-      mem_write: begin
-        if (fast_path || !mem_busy) begin
-          state <= write_back;
+          state <= mem_read;
         end
       end
 
       write_back: begin
-        if (d1) begin // Write to A register?
-          a_reg <= alu_out;
-        end
+        if (!d3 || fast_path || !mem_busy) begin
+          if (d1) begin // Write to A register?
+            a_reg <= alu_out;
+          end
 
-        if (d2) begin // Write to D register?
-          d_reg <= alu_out;
-        end
+          if (d2) begin // Write to D register?
+            d_reg <= alu_out;
+          end
 
-        if (jump) begin
-          prog_counter <= a_reg;
-        end else begin
-          prog_counter <= prog_counter + 1;
-        end
+          if (jump) begin
+            prog_counter <= a_reg;
+          end else begin
+            prog_counter <= prog_counter + 1;
+          end
 
+          state <= mem_read;
+        end
+      end
+
+      mem_read: begin
+        if (fast_path) begin
+          state <= inst_fetch;
+        end else if (!mem_busy) begin
+          state <= mem_wait;
+        end
+      end
+
+      mem_wait: begin
+        state <= mem_wait_1;
+      end
+
+      mem_wait_1: begin
         state <= inst_fetch;
       end
     endcase
