164f8bfb4722ccd5575c07a7f8da30aabbee3d60
Leo Cassarani <me@leocassarani.net>
2020-06-10 21:02:38 +0100

    Implement a more faithful SPI flash cosimulator
    In particular, we now shift bits into the MISO pin during the negative,
    rather than the positive, SPI clock edge.

--- sim/flash.cpp
+++ sim/flash.cpp
@@ -1,46 +1,101 @@
 #include "flash.h"
 
+// The program ROM starts at 1024KiB, so we want to ensure that the SPI master
+// is trying to access the correct base address.
+const uint32_t FlashBaseAddr = 0x100000;
+
 void Flash::tick(uint8_t spi_cs, uint8_t spi_clk, uint8_t spi_mosi, uint8_t& spi_miso)
 {
     // The Chip Select (CS) signal is active low, so if it's high,
     // we can reset our state and bail out early.
     if (spi_cs) {
-        streaming = false;
+        state = FlashState::Idle;
         count = 0;
         return;
     }
 
-    if (posedge(spi_clk)) {
-        if (streaming) {
-            if (count == 0) {
-                std::string str;
-                std::getline(file, str);
+    edge_detect(spi_clk);
 
-                if (file.good()) {
-                    // Convert the string to an integer in base 2.
-                    int i = std::stoi(str, nullptr, 2);
-                    word = static_cast<uint16_t>(i);
-                } else {
-                    word = 0xFF;
-                }
-            }
+    switch (state) {
+    case FlashState::Idle:
+        if (spi_posedge) {
+            spi_read(spi_mosi);
 
-            spi_miso = 0x01 & (word >> (15 - count));
+            // Read the 8-bit command (which we expect to be 0x03).
+            if (++count > 7) {
+                if ((rbuf & 0x000000FF) == 0x03)
+                    state = FlashState::Command;
 
-            if (++count > 15)
                 count = 0;
-        } else {
-            if (++count > 30) {
-                count = 0;
-                streaming = true;
             }
         }
+
+        break;
+    case FlashState::Command:
+        if (spi_posedge) {
+            spi_read(spi_mosi);
+
+            // Read the 24-bit flash address then start streaming data.
+            if (++count > 23) {
+                // Check that we get the expected address, abort otherwise.
+                if ((rbuf & 0x00FFFFFF) == FlashBaseAddr) {
+                    state = FlashState::Streaming;
+                } else {
+                    state = FlashState::Idle;
+                }
+
+                count = 0;
+            }
+        }
+
+        break;
+    case FlashState::Streaming:
+        if (spi_negedge) {
+            if (count == 0)
+                fill_wbuf();
+
+            spi_write(spi_miso);
+
+            // Write 16 bits at a time before filling the write buffer with the
+            // next word from the file on disk.
+            if (++count > 15)
+                count = 0;
+        }
+
+        break;
     }
 }
 
-bool Flash::posedge(uint8_t spi_clk)
+void Flash::fill_wbuf()
 {
-    bool posedge = spi_clk && !spi_clk_prev;
+    string str;
+    std::getline(file, str);
+
+    if (file.good()) {
+        // Convert the string to an integer in base 2.
+        int i = std::stoi(str, nullptr, 2);
+        wbuf = static_cast<uint16_t>(i);
+    } else {
+        // If we've reached past the end of the file, return 0xFF to mimic the
+        // behaviour of the W25Q128JV chip.
+        wbuf = 0xFF;
+    }
+}
+
+void Flash::edge_detect(uint8_t spi_clk)
+{
+    spi_posedge = spi_clk && !spi_clk_prev;
+    spi_negedge = !spi_clk && spi_clk_prev;
     spi_clk_prev = spi_clk;
-    return posedge;
+}
+
+void Flash::spi_read(uint8_t spi_mosi)
+{
+    rbuf = (spi_mosi & 0x01) | (rbuf << 1);
+}
+
+void Flash::spi_write(uint8_t& spi_miso)
+{
+    // We need to write the MSB first due to the on-disk layout of the ROM.
+    spi_miso = 0x01 & (wbuf >> (15 - count));
 }
--- sim/flash.h
+++ sim/flash.h
@@ -4,22 +4,40 @@
 #include <fstream>
 #include <string>
 
+using std::string;
+
+enum class FlashState {
+    Idle,
+    Command,
+    Streaming,
+};
+
 class Flash {
 public:
-    Flash(const std::string& filename)
+    Flash(const string& filename)
         : file(std::ifstream(filename)) {};
 
     void tick(uint8_t spi_cs, uint8_t spi_clk, uint8_t spi_mosi, uint8_t& spi_miso);
 
 private:
     std::ifstream file;
+    FlashState state = FlashState::Idle;
 
-    uint8_t spi_clk_prev = 0;
-    bool posedge(uint8_t spi_clk);
-
-    bool streaming = false;
     unsigned count = 0;
-    uint16_t word = 0;
+
+    void edge_detect(uint8_t spi_clk);
+    uint8_t spi_clk_prev = 0;
+
+    bool spi_posedge = false;
+    bool spi_negedge = false;
+
+    void spi_read(uint8_t spi_mosi);
+    void spi_write(uint8_t& spi_miso);
+
+    uint32_t rbuf;
+    uint16_t wbuf;
+
+    void fill_wbuf();
 };
 
 #endif
